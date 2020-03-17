4f99f491e78788b5433cf91930fd4245e6a41d13
Robert Šefman <robert@epodjetnik.com>
2020-03-17 21:41:31 +0100

    Updates

---
+++ precache-manifest.5ec04c7156ddfe56d87ec9281a53e0a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c7451c9a7ec0b767ee2dc6fcb6eb12",
+    "url": "/corona-simulation/index.html"
+  },
+  {
+    "revision": "17354c3d173fd21f1111",
+    "url": "/corona-simulation/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "23d34f38ce2e8bf433b2",
+    "url": "/corona-simulation/static/js/2.aafc1f57.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/corona-simulation/static/js/2.aafc1f57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17354c3d173fd21f1111",
+    "url": "/corona-simulation/static/js/main.aab1159a.chunk.js"
+  },
+  {
+    "revision": "0a2024bfa65d407c2673",
+    "url": "/corona-simulation/static/js/runtime-main.57d57848.js"
+  }
+]);
